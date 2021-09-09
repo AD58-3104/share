762a4cb0152be37ec9b752f0c144d43dd7a4de28
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2021-09-09 23:56:40 +0900

    sampleを変更

--- clientmain.cpp
+++ clientmain.cpp
@@ -6,8 +6,9 @@ int main(int argc, char const *argv[])
     Client client("127.0.0.1",7110);
 
     
+    std::string s("citbrains");
     for(int i = 0;i < 5;++i)
-        client.send(std::string("fuck"));
+        client.send(std::move(s));
     client.send(std::string("end"));
     std::cout << "running\n";
     std::cout << "\n\nend !!!!!!!!!!!!!!!!!!!!!!!!!!!";
